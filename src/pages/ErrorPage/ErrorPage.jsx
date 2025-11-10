0ff39987a1c18d338d8c070509a4c75adba68640
Asif-vai <mr3366533@gmail.com>
2025-11-10 21:16:46 +0600

    update 404 page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,48 +1,44 @@
 import React from "react";
-import { motion } from "framer-motion";
 import { Link } from "react-router";
+import { motion } from "framer-motion";
 
 function ErrorPage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 text-center px-4">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-base-200 text-base-content px-4 text-center">
       <motion.h1
-        initial={{ opacity: 0, y: -50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6 }}
-        className="text-[120px] font-extrabold text-[#FF2EFF] drop-shadow-lg"
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{ type: "spring", stiffness: 100, damping: 10 }}
+        className="text-9xl font-extrabold text-[#FF2EFF] drop-shadow-lg"
       >
         404
       </motion.h1>
 
       <motion.h2
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
+        initial={{ opacity: 0, y: 30 }}
+        animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.3 }}
-        className="text-2xl md:text-3xl font-semibold text-gray-800 mb-3"
+        className="text-3xl md:text-4xl font-semibold mt-4"
       >
-        Oops! Page Not Found
+        Oops! Page not found
       </motion.h2>
 
       <motion.p
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 0.5 }}
-        className="text-gray-600 max-w-md mb-8"
+        initial={{ opacity: 0, y: 30 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.6 }}
+        className="text-sm md:text-base mt-2 text-gray-500 dark:text-gray-400 max-w-md"
       >
-        The page you’re looking for doesn’t exist or has been moved.  
-        But don’t worry, let’s get you back on track!
+        The page you’re looking for doesn’t exist or has been moved to another location.
       </motion.p>
 
       <motion.div
-        initial={{ opacity: 0, scale: 0.9 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ delay: 0.7 }}
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.9 }}
       >
-        <Link
-          to="/"
-          className="px-6 py-3 text-white rounded-full shadow-md btn-style"
-        >
-          Go Back Home
+        <Link to="/" className="btn btn-style mt-6">
+           Back to Home
         </Link>
       </motion.div>
     </div>
